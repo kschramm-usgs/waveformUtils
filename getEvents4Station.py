562cabe0fc3db747445d3b41f109313aeda66d65
Kimberly Schramm <kschramm@usgs.gov>
2017-04-25 13:19:37 -0600

    add script to pull info from event catalog

--- getEvents4Station.py
+++ getEvents4Station.py
@@ -1,30 +1,23 @@
 #!/bin/env python
+def getEvents4Station(stLat,stLon,startTime,endTime,minRad,maxRad,minMag):
+    ''' This will collect a set of events for a station.
+        input: station latitude, station longitude, 
+               starting and ending times(UTC date time),
+               minimum and maximum degrees distance from station, 
+               minimum magnitude
+        output: a catalog of events
+    '''
+    from obspy import UTCDateTime
+    from obspy.clients.fdsn import Client
 
-from obspy import UTCDateTime
-from obspy.clients.fdsn import Client
-from obspy.core.event import Origin
-import returnDistDeg
+    #stLat = 34.945910
+    #stLon = -106.457200
+    #startTime = UTCDateTime("2016-01-01T00:00:00")
+    #endTime = UTCDateTime("2017-04-24T00:00:00")
 
-stLat = 34.945910
-stLon = -106.457200
-stDepth = -1820
-
-net = "IU"
-sta = ["ANMO"]
-chan = "00"
-comp = "BH*"
-debug="True"
-
-client = Client("IRIS")
-
-startTime = UTCDateTime("2016-01-01T00:00:00")
-endTime = UTCDateTime("2017-04-24T00:00:00")
-
-EventCatalog = client.get_events(starttime=startTime,endtime=endTime,\
+    client = Client("IRIS")
+    EventCatalog = client.get_events(starttime=startTime,endtime=endTime,\
                                  latitude=stLat,longitude=stLon, \
-                                 minradius=25, maxradius=90, \
-                                 minmagnitude=6.0)
-
-print(EventCatalog)
-for event in EventCatalog:
-    print(event.origins[0]['time'])
+                                 minradius=minRad, maxradius=maxRad, \
+                                 minmagnitude=minMag)
+    return EventCatalog
---
+++ getInfoFromCatalog.py
@@ -0,0 +1,33 @@
+#!/bin/env python
+''' This script contains examples of how to get information out of a
+    catalog built by the fdsn iris client
+'''
+from obspy import UTCDateTime
+from obspy.clients.fdsn import Client
+from obspy.core.event import Origin
+import returnDistDeg
+from getEvents4Station import getEvents4Station 
+
+client = Client("IRIS")
+
+net = "IU"
+sta = "ANMO"
+staLat = 34.945910
+staLon = -106.457200
+staDepth = -1820
+
+startTime = UTCDateTime("2016-01-01T00:00:00")
+endTime = UTCDateTime.now() # use current time
+minRad=25
+maxRad=90
+minMag=6.0
+
+EventCatalog = getEvents4Station(staLat,staLon,startTime,endTime,minRad,maxRad,minMag)
+
+print(EventCatalog)
+for event in EventCatalog:
+    print(event.origins[0]['time'])
+    time = event.origins[0]['time']
+    doy=time.julday
+    print doy
+    
