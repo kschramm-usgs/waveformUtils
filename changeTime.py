5bbf6fbd9c2873e8bdbd1284cce11c4c761b3183
Schramm <kschramm@usgs.gov>
2017-08-31 13:23:47 -0600

    change time

--- changeTime.py
+++ changeTime.py
@@ -5,6 +5,7 @@ from obspy import UTCDateTime
 from obspy.core import *
 from obspy import read
 import glob
+from obspy.mseed.util import shift_time_of_file
 
 stime=UTCDateTime('2017-242T13:38:00.0Z')
 etime=UTCDateTime('2017-242T13:53:00.0Z')
@@ -18,42 +19,10 @@ station = "TPNV"
 channel = "CB"
 component = "BC0"
 prefix = 'HF'
+other='shifted'
+cut='cut'
 
-dataloc="/tr1/telemetry_days/"
-#    /tr1/telemetry_days/XX_FBA1/2017/2017_151
+fileIn=prefix+"_"+station+"_"+channel + "_" + component +".512."+cut+".seed"
+fileOut=prefix+"_"+station+"_"+channel + "_" + component +".512."+other+".seed"
 
-fileName = []
-for year in range(stime.year, etime.year+1):
-    for day in range(stime.julday, etime.julday+1):
-        string = dataloc + network + '_' + station + '/' + str(year) + '/*' \
-                 + str(year) + '_' + str(day).zfill(3)+\
-                 '/*' + channel + '*' + component + '*.seed'
-        print(string)
-        fileName += glob.glob(string)
-
-st = Stream()
-for curfile in fileName:
-    try:
-        if stime.julday != etime.julday:
-            print('reading in file: '+curfile)
-            st += read(curfile)
-        else:
-            st += read(curfile,starttime=stime,endtime=etime)
-#        st += read(curfile)
-    except:
-        print('Unable to open file: '+ str(curfile))
-
-if stime.julday != etime.julday:
-    st2=st.copy()
-    st2.merge(fill_value='latest')
-    st2.trim(starttime=stime,endtime=etime)
-    st2.plot()
-    st = st2
-
-st.plot()
-:q
-
-
-fileName=prefix+"_"+station+"_"+channel + "_" + component +".512.cut.seed"
-print(fileName)
-st.write(fileName,format='MSEED',reclen=512)
+shift_time_of_file(fileIn, fileOut, 0.005)
